a5462b6d547029536dd51be5d081786869ec9c8d
shlifedev <shlifedev>
2025-09-07 23:09:38 +0900

    Initial commit

---
+++ content/posts/dev/my-first-post.md
@@ -0,0 +1,5 @@
++++
+date = '2025-09-07T23:04:36+09:00'
+draft = true
+title = 'My First Post'
++++
---
+++ theme.sh
@@ -0,0 +1,2 @@
+git submodule add --depth=1 https://github.com/adityatelange/hugo-PaperMod.git themes/PaperMod
+git submodule update --init --recursive # needed when you reclone your repo (submodules may not get cloned automatically)
